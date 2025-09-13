4ec78837c521703d4a5cc4fcfe2ad379db5c3249
Steven Zhang <stevenzhang@MacBook-Pro-102.local>
2025-09-13 14:53:01 -0400

    map view

--- frontend/yourfoodie/App.js
+++ frontend/yourfoodie/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function App() {
+export default function MapScreen() {
+  const mapRef = useRef(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <MapView
+        ref={mapRef}
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
